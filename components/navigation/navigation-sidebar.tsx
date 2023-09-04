b4856a54bdec5f66d93f5bbedc7ed92e708253eb
flaming-streamz <flamingstreamz@gmail.com>
2023-09-04 03:15:19 +0300

    listing servers for this user in nav sidebar, theme toggler button and user button added

--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -1,9 +1,15 @@
 import { redirect } from "next/navigation";
+import { UserButton } from "@clerk/nextjs";
 
 import { db } from "@/lib/db";
 import { currentProfile } from "@/lib/current-profile";
 
+import { Separator } from "@/components/ui/separator";
+import { ScrollArea } from "@/components/ui/scroll-area";
+
 import { NavigationAction } from "./navigation-action";
+import { NavigationItem } from "./navigation-item";
+import { ModeToggle } from "@/components/mode-toggle";
 
 interface Props {}
 
@@ -16,6 +22,25 @@ const NavigationSidebar = async (props: Props) => {
   return (
     <div className="space-y-4 flex flex-col items-center h-full text-primary w-full dark:bg-[#1E1F22] py-3">
       <NavigationAction />
+      <Separator className="h-[2px] bg-zinc-300 dark:bg-zinc-700 rounded-md w-[80%] mx-auto" />
+      <ScrollArea className="flex-1 w-full">
+        {servers.map((server) => (
+          <div key={server.id} className="mb-3">
+            <NavigationItem id={server.id} imageUrl={server.imageUrl} name={server.name} />
+          </div>
+        ))}
+      </ScrollArea>
+      <div className="pb-3 mt-auto flex items-center flex-col gap-y-4">
+        <ModeToggle />
+        <UserButton
+          afterSignOutUrl="/"
+          appearance={{
+            elements: {
+              avatarBox: "h-[36px] w-[36px]",
+            },
+          }}
+        />
+      </div>
     </div>
   );
 };
